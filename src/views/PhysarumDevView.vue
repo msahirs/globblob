e84a1e8a42217133d068e4691444d275e382ffc9
Sahir Sujahudeen <m.s.sujahudeen@student.tudelft.nl>
2026-01-29 21:14:40 +0100

    Added physarum sim

---
+++ src/views/PhysarumDevView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import PhysarumSim from '@/components/PhysarumSim.vue'
+</script>
+
+<template>
+  <main class="phys-view">
+    <PhysarumSim />
+  </main>
+</template>
+
+<style scoped>
+.phys-view {
+  position: fixed;
+  inset: 0;
+}
+</style>
+
